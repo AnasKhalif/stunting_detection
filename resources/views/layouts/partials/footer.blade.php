c6b53ea65e2cf65958c95eaf6e8a3ac6e118fbe6
AnasKhalif <anaskhalif995@gmail.com>
2024-10-12 08:00:44 +0700

    feat : add template dashboard

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,16 @@
+<footer>
+    <p class="text-base text-gray-400 font-normal p-3 text-center">
+        Design and Developed by <a href="https://www.wrappixel.com/" target="_blank"
+            class="text-blue-600 underline hover:text-blue-700">wrappixel.com</a>
+    </p>
+</footer>
+</div>
+
+
+</main>
+<!-- Main Content End -->
+
+</div>
+</div>
+<!--end of project-->
+</main>
